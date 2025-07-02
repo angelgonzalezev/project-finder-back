ee7bfafc215abd193908d2879539ac51e60b1f9a
Angel Gonzalez <aangel.gg@hotmail.com>
2025-07-02 22:07:59 +0200

    Try open cors

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,7 @@ import 'reflect-metadata';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: 'https://project-finder-frontend.vercel.app',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  });
+  app.enableCors({ origin: true, credentials: true });
 
   await app.listen(process.env.PORT ?? 3000);
 }
