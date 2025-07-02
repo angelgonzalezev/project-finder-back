8e5534486e39ddf912afea8b42ff49e878cd0b4d
Angel Gonzalez <aangel.gg@hotmail.com>
2025-07-02 21:55:53 +0200

    Enable cors for frontend

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ import 'reflect-metadata';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://project-finder-frontend.vercel.app',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
